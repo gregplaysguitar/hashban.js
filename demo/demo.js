f37b2d7ebc56e703d4fde46291f56befeadef100
Greg Brown <gregplaysguitar@gmail.com>
2014-04-07 10:39:41 +1200

    Updated demo for new API

---
+++ demo/demo.js
@@ -0,0 +1,47 @@
+(function($) {
+    $(function() {
+
+        function Transition() {
+            // Define a custom transition class, using js pseudo-inheritance to 
+            // inherit methods from Hashban.DefaultTransition
+
+            // Apply the parent's constructor
+            Hashban.DefaultTransition.apply(this, arguments);
+
+            // Provide custom transition_in behaviour
+            this.transition_in = function(new_content, contentBody) {
+                this.old_content.remove();
+                new_content.css({
+                    position: 'relative',
+                    top: 1000
+                }).show().animate({
+                    top: 0
+                }, 300);
+            };
+
+            // Other functions which could be overridden:
+            // this.should_use = function() {
+            //     // return a bool
+            // };
+            // this.transition_out = function(endfade) {
+            //     // perform out transition then call endfade callback
+            // };
+        };
+        // Inherit non-overridden methods from parent
+        Transition.prototype = new Hashban.DefaultTransition();
+
+        function content_links() {
+            hashban.hijack($('#content'));
+        };
+
+        var hashban_options = {
+                contentWrapSelector: '#content',
+                transitions: [Transition],
+                content_init: content_links
+            },
+            hashban = Hashban(hashban_options);
+        hashban.hijack($('#header nav'));
+        hashban.bind();
+        content_links();
+    });
+})(jQuery);
